8d9dc25c4472fc92607353082cb3f400fb8a3eee
Lewis Cowles <lewiscowles@me.com>
2018-07-18 17:49:11 +0100

    completed task 1

--- point.cpp
+++ point.cpp
@@ -9,3 +9,37 @@ bool operator==(const point& lhs, const point& rhs)
     return lhs.x == rhs.x &&
             lhs.y == rhs.y;
 }
+
+bool operator!=(const point& lhs, const point& rhs)
+{
+  return !(lhs == rhs);
+}
+
+point operator+(const point& lhs, const point& rhs)
+{
+  return point {lhs.x + rhs.x, lhs.y + rhs.y};
+}
+point operator-(const point& lhs, const point& rhs)
+{
+  return point {lhs.x - rhs.x, lhs.y - rhs.y};
+}
+
+point& point::operator+=(const point& other)
+{
+    this->x += other.x;
+    this->y += other.y;
+    return *this;
+}
+
+point& point::operator-=(const point& other)
+{
+    this->x -= other.x;
+    this->y -= other.y;
+    return *this;
+}
+
+std::ostream& operator<<( std::ostream& out, const point& p )
+{
+    out << "(" << p.x << ", " << p.y << ")";
+    return out;
+}
--- point.hpp
+++ point.hpp
@@ -1,4 +1,4 @@
-
+#include <iostream>
 
 #ifndef WEEK11_POINT_EXERCISE_POINT_HPP
 #define WEEK11_POINT_EXERCISE_POINT_HPP
@@ -6,8 +6,17 @@
 struct point {
     int x = 0;
     int y = 0;
+
+    point& operator+=(const point& other);
+    point& operator-=(const point& other);
 };
 
 bool operator==(const point& lhs, const point& rhs);
+bool operator!=(const point& lhs, const point& rhs);
+
+point operator+(const point& lhs, const point& rhs);
+point operator-(const point& lhs, const point& rhs);
+
+std::ostream& operator<<( std::ostream& output, const point& p );
 
 #endif //WEEK11_POINT_EXERCISE_POINT_HPP
